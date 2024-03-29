cb80d42f5790e04a094de847570b29b83212b095
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-03-29 18:12:28 +0530

    Add files via upload

---
+++ TRUTHY AND FALSY.js
@@ -0,0 +1,41 @@
+const userInput=prompt("Enter something");
+
+if(userInput)
+
+{
+
+    console.log("Truthy");
+}
+
+else
+
+{
+    console.log("Falsy");
+}
+
+
+
+if(0)
+
+{
+    console.log("TRUTHY");
+}
+
+else
+{
+    console.log("FALSY");
+}
+
+
+
+if("")
+
+{
+    console.log("TRUTHY");
+}
+
+else
+{
+    console.log("FALSY");
+}
+
